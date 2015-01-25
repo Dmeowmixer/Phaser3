8ed98998e7ab8b715b878bb59d2432f12d23ef0d
dmeowmixer <dmeowmixer@hotmail.com>
2015-01-25 10:20:47 -1000

    doing sh

--- app.js
+++ app.js
@@ -1,52 +1,108 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+ 
+function preload() {
+  game.load.image('enemy', 'assets/baddie.png');
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', 'assets/Isshin.png', 70, 60);
+}
+var player;
+var platforms;
+
+function create() {
+  game.add.sprite(0, 5,'enemy');
+  game.add.sprite(0, 0, 'dude');
+  //  We're going to be using physics, so enable the Arcade Physics system
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  //  A simple background for our game
+  game.add.sprite(0, 0, 'sky');
+
+  //  The platforms group contains the ground and the 2 ledges we can jump on
+  platforms = game.add.group();
+
+  //  We will enable physics for any object that is created in this group
+  platforms.enableBody = true;
+
+  // Here we create the ground.
+  var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  ground.scale.setTo(2, 2);
+
+  //  This stops it from falling away when you jump on it
+  ground.body.immovable = true;
+
+  //  Now let's create two ledges
+  var ledge = platforms.create(400, 400, 'ground');
+
+  ledge.body.immovable = true;
+
+  ledge = platforms.create(-150, 250, 'ground');
+
+  ledge.body.immovable = true;
+  // The player and its settings
+  player = game.add.sprite(32, game.world.height - 150, 'dude');
+  enemy = game.add.sprite(32, game.world.height - 150, 'enemy');
+
+  //  We need to enable physics on the player
+  game.physics.arcade.enable(player);
+
+  game.physics.arcade.enable(enemy);
+  enemy.body.bounce.y = 0.2;
+  enemy.body.gravity.y = 300;
+  enemy.body.collideWorldBounds = true;
+  //  Player physics properties. Give the little guy a slight bounce.
+  player.body.bounce.y = 0.2;
+  player.body.gravity.y = 300;
+  player.body.collideWorldBounds = true;
+
+  //  Our two animations, walking left and right.
+  player.animations.add('left', [0, 1, 2, 3], 8, true);
+  player.animations.add('right', [6, 7, 8, 9], 8, false);
+  player.animations.add('attack',[ 20, 21, 22, 23, 24], 2, true);
+  player.animations.add('jump', [15,16,17,18], 10, true);
+  cursors = game.input.keyboard.createCursorKeys();
+
+}
+ 
+function update() {
+  game.physics.arcade.collide(player, platforms);
+ //  Reset the players velocity (movement)
+  player.body.velocity.x = 0;
 
 
-   // playState = {
-   //  init: function() {
-   //  game.state.add('play', playState);
-   //  game.state.start('play');
+  if (cursors.left.isDown)
+  {
+      //  Move to the left
+      player.body.velocity.x = -150;
 
-   //  //Called as soon as we enter this state
-   //  }
-
-    function preload(){
-      game.load.image('sky', 'assets/sky.png');
-      game.load.image('ground', 'assets/platform.png');
-      game.load.image('star', 'assets/star.png');
-      game.load.spritesheet('dude', '/assets/dude.png', 32, 48);
-      game.stage.backgroundColor = '#6d94b5';
-
-    //Assets to be loaded before create() is called
-    }
-
-    function create(){
-      game.physics.startSystem(Phaser.Physics.ARCADE);
-
-      //Adding sprites, sounds, etc...
-      //image, sprite, audio and others are all methods of the factory
-      game.add.sprite(0,0,'sky');
-      game.add.sprite(0, 570 ,'ground');
-      game.add.sprite(400, 570, 'ground');
-      player = game.add.sprite(0,525, 'dude');
-      game.physics.enable(player, Phaser.Physics.ARCADE);
-      var arrow =  game.input.keyboard.createCursorKeys();
-
-    }
-
-  function update(){
-    if(arrow.left.isDown){
-      console.log('keypress left');
-      player.body.velocity.x -= 2;
-      return;
-    }
-    else if(arrow.right.isDown){
-      console.log('keypress right');
-      player.body.velocity.x += 2;
-      return;
-    }
-    if (arrow.up.isDown){
-      console.log('keypress up');
-      player.body.y -= 2;
-      return;
-    }
+      player.animations.play('left');
   }
+  else if (cursors.right.isDown)
+  {
+      //  Move to the right
+      player.body.velocity.x = 150;
+
+      player.animations.play('right');
+  }
+  else
+  {
+      //  Stand still
+      player.animations.stop();
+
+      player.frame = 4;
+  }
+  
+  //  Allow the player to jump if they are touching the ground.
+  if (cursors.up.isDown && player.body.touching.down)
+  {
+      player.animations.play('jump');
+      player.body.velocity.y = -350;
+  }
+  if (cursors.down.isDown){
+    player.animations.play('attack');
+    player.body.velocity.x = 0;
+  }
+}
